944f363f924594d04fbe0511e11cc21a1ed24e29
Ben Anderson <ben.anderson@healthcatalyst.com>
2021-12-08 14:33:47 -0700

    add dist and minimize tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  purge: ["./*.html", "./src/**/*.js"],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    screens: {
-      sm: {'min': '0px', 'max': '767px'},
-      md: "768px",
-      lg: {'min': '1024px'},
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwindcss-debug-screens')
-  ],
+	purge: {
+		enabled: true,
+		content: ['./*.html', './src/**/*.js']
+	},
+	darkMode: 'media', // or 'media' or 'class'
+	theme: {
+		screens: {
+			sm: { min: '0px', max: '767px' },
+			md: '768px',
+			lg: { min: '1024px' }
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
 };
