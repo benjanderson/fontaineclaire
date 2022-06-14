b7639a5769bcd3b8efda8af05eb27b4033bd21fe
Ben Anderson <ben.anderson@healthcatalyst.com>
2022-06-14 08:51:44 -0600

    stake conference

--- configuration/webpack.dev.config.js
+++ configuration/webpack.dev.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 
 const webpackConfiguration = require('../webpack.config');
 const environment = require('./environment');
+const CnameWebpackPlugin = require('cname-webpack-plugin');
 
 module.exports = merge(webpackConfiguration, {
   mode: 'development',
@@ -34,5 +35,9 @@ module.exports = merge(webpackConfiguration, {
   },
 
   /* Additional plugins configuration */
-  plugins: [],
+  plugins: [
+    new CnameWebpackPlugin({
+      domain: 'fontaineclaire.com',
+    }),
+  ],
 });
