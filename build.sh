7c5b3b3ac00090c964b85246ea76a7f100f5af26
Ben Anderson <1934453+benjanderson@users.noreply.github.com>
2025-09-15 09:50:26 -0600

    update hugo download url

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 main() {
     HUGO_VERSION="0.150.0"
 
     # Install Hugo
-    curl -LO https://github.com/goharbor/harbor/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-    tar -xzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-    mv hugo /usr/local/bin/
-    rm -f hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+    curl -LO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
+    tar -xzf "hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
+    mv "hugo" /usr/local/bin/
+    rm -f "hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
 
     # Verify installation
     if command -v hugo &> /dev/null
@@ -21,5 +23,4 @@ main() {
     hugo --gc --minify
 }
 
-set -euo pipefail
 main "$@"
