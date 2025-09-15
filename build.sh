b667e5a2a146583a5010eb17e9b4472241a6bd12
Ben Anderson <1934453+benjanderson@users.noreply.github.com>
2025-09-15 09:52:34 -0600

    install hugo locally

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ main() {
     HUGO_VERSION="0.150.0"
 
     # Install Hugo
+
+    mkdir -p bin
     curl -LO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
-    tar -xzf "hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
-    mv "hugo" /usr/local/bin/
+    tar -xzf "hugo_${HUGO_VERSION}_linux-amd64.tar.gz" -C bin --strip-components=1
     rm -f "hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
+    export PATH="$PWD/bin:$PATH"
 
     # Verify installation
     if command -v hugo &> /dev/null
